7e2e53162023a2d1e153ac6c1f9241131bb2b245
Raj Singh <rs457036@gmail.com>
2023-12-10 14:01:57 +0000

    fix: theme_switcher.js

--- scripts/theme_switcher.js
+++ scripts/theme_switcher.js
@@ -1,36 +1,37 @@
 
 (() => {
     const _id = "attricss";
-    const link = document.querySelector(`#${_id}`);
-    const buildAttriCssLinkTag = (theme, link) => {
-        let update = link != null;
-        const link = link || document.createElement("link");
+    const buildAttriCssLinkTag = (theme) => {
+        let link = document.querySelector(`#${_id}`);
+        
+        if (!link) {
+            link = document.createElement("link");
+            link.id = _id;
+            link.rel = "stylesheet";
+            document.head.appendChild(link);
+        }
+        
 
         if (theme === "dark") {
             link.href = "./styles/attricss/darkforest-green.css";
         } else {
             link.href = "./styles/attricss/brightlight-green.css"
         }
-
-        if (!update) {
-            link.id = _id;
-            link.rel = "stylesheet";
-            document.head.appendChild(link);
-        }
     }
     
     const switchTheme = (event) => {
         if (event.matches) {
             // enable dark theme styling
-            buildAttriCssLinkTag("dark", link);
+            buildAttriCssLinkTag("dark");
         } else {
-            buildAttriCssLinkTag("light", link);
+            buildAttriCssLinkTag("light");
         }
     }
     
     const startListening = () => {
         const mediaMatch = window.matchMedia("(prefers-color-scheme: dark)");
-        mediaMatch.addEventListener(switchTheme);
+        switchTheme(mediaMatch);
+        mediaMatch.addEventListener("change", switchTheme);
     }
 
     startListening();
